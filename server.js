b39d9049475941d5235aa02cd78ad93cfd49015b
sam <sammybonfanti@gmail.com>
2019-11-14 02:34:29 -0700

    28 connection node to database

--- server.js
+++ server.js
@@ -1,15 +1,88 @@
+require('dotenv').config();
+
 // depandencies
-let express = require('express')
+const express = require('express'),
+      mongodb = require('mongodb');
+
+const {
+    CONNECTION_STRING,
+    SERVER_PORT
+} = process.env;
 
 // connect to mongoDB
+let db
+let connectionString = CONNECTION_STRING;
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
+    db = client.db();
+    // dont let db run until its done then run server
+    app.listen(SERVER_PORT);
+})
 
 // express
-let app = express()
+let app = express();
+app.use(express.urlencoded({extended: false}));
 
 // routes
 app.get('/', function(req, res) {
-    res.send('hello, welcome to our app!')
-})
+    res.send(`
+        <!DOCTYPE html>
+            <html>
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Simple To-Do App</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+            </head>
+            <body>
+            <div class="container">
+                <h1 class="display-4 text-center py-1">To-Do App</h1>
+
+                <div class="jumbotron p-3 shadow-sm">
+                <form action='/create-item' method='post'>
+                    <div class="d-flex align-items-center">
+                    <input name='item' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                    <button class="btn btn-primary">Add New Item</button>
+                    </div>
+                </form>
+                </div>
+
+                <ul class="list-group pb-5">
+                <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                    <span class="item-text">Fake example item #1</span>
+                    <div>
+                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    </div>
+                </li>
+                <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                    <span class="item-text">Fake example item #2</span>
+                    <div>
+                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    </div>
+                </li>
+                <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+                    <span class="item-text">Fake example item #3</span>
+                    <div>
+                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                    </div>
+                </li>
+                </ul>
+
+            </div>
+
+            </body>
+            </html>
+        `)
+});
+
+app.post('/create-item', function (req, res) {
+    // select the database collection
+    db.collection('items').insertOne({text: req.body.item}, function () {
+        res.send('thanks for sub the form.')
+    });
+});
 
 // start server
-app.listen(3000)
