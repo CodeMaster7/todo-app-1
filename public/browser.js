65f1e9039f2f290c1c6159326fb20600e12c517f
sam <sammybonfanti@gmail.com>
2019-11-15 22:53:16 -0700

    32 updating a database item

--- public/browser.js
+++ public/browser.js
@@ -1,11 +1,13 @@
 document.addEventListener('click', function (e) {
     // only the element that was clicked on contains a class of edit-me
     if (e.target.classList.contains('edit-me')) {
-        let userInput = prompt('enter your desired new text')
-        axios.post('/update-item', {text: userInput}).then(function () {
-            // do something interesting here in the next video
+        let userInput = prompt('enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML)
+        if (userInput) {
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(function () {
+            e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput
         }).catch(function () {
             console.log('please try again later');
         })
+        }
     }
 })
